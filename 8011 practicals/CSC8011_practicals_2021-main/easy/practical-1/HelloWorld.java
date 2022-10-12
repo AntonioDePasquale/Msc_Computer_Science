21adb2b13ddce8572556687ad1c2adcb73b32399
AntonioDePasquale <AntonioDePasqualeUK@gmail.com>
2022-10-12 15:52:49 +0100

    init

---
+++ 8011 practicals/CSC8011_practicals_2021-main/easy/practical-1/HelloWorld.java
@@ -0,0 +1,61 @@
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+
+       //Problem 1.1
+
+       //System.out.println("Hello World");
+
+        //Problem 1.2
+
+       /* int x = 100;
+        int y = 50;
+        int sum = x + y;
+
+        System.out.println(sum);*/
+
+        //Problem 1.2.2
+
+        /*int x = -22 + 3 * 2;
+        int y = (9/2) % 9;
+        int z = 2147483646 + -15 / 2;
+        int q = (-2147483646 + -1000/ 2) * 10;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(q);*/
+
+        //Problem 1.3
+
+      /*  int x = 250;
+        int y = 72;
+
+        int sum = x + y;
+        int multiply = x * y;
+        int subtract = x - y;
+        int divide = x / y;
+        int remainder = x % y;
+
+        System.out.println(sum);
+        System.out.println(multiply);
+        System.out.println(subtract);
+        System.out.println(divide);
+        System.out.println(remainder);
+*/
+
+        addition(); //ignore for now
+        fancyPrint(); // ignore for now
+    }
+    
+    public static void addition() {
+
+    }
+    
+    public static void fancyPrint() {
+        System.out.println("   J    a   v     v  a   ");
+        System.out.println("   J   a a   v   v  a a  ");
+        System.out.println("J  J  aaaaa   V V  aaaaa ");
+        System.out.println(" JJ  a     a   V  a     a");
+    }
+}
