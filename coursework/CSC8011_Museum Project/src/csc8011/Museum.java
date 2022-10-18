06f0b4139f998d87a202b3c66f1ac8da3e0a067d
AntonioDePasquale <AntonioDePasqualeUK@gmail.com>
2022-10-18 23:57:49 +0100

    csc8011 project init

--- coursework/CSC8011_Museum Project/src/csc8011/Museum.java
+++ coursework/CSC8011_Museum Project/src/csc8011/Museum.java
@@ -24,26 +24,57 @@ public class Museum {
         return exhibits.remove(i);
     }
 
-    //function getter for Museum name//
+    //function getter and setter for Museum name//
 
     public String getMuseumName() {
         return name;
     }
+    public void setMuseumName(String newName) {
+        this.name = newName;
+    }
 
     //function to the find the highest value of all objects in the arraylist, then returns that object//
 
-    public Object getHighestValue() {
+    public Object findHighestValue() {
         Integer length = exhibits.size();
         Exhibit highestValExhibit = new Exhibit(0,"default",0,0.00);
         double max = 0.0;
 
-        for (Exhibit exhibit : exhibits) {
-            if (exhibit.getValue() > max) {
-                highestValExhibit = exhibit;
+        for (int i = 0; i < length; i++) {
+            if (exhibits.get(i).getValue() > max) {
+                highestValExhibit = exhibits.get(i);
             }
         }
         return highestValExhibit;
     }
 
     //function to find object representing the first item acquired//
+
+    public Object findFirstAdded() {
+        Integer length = exhibits.size();
+        Exhibit firstExhibit = new Exhibit(0,"default",0,0.00);
+        Integer year = 9999;
+
+        for (int i = 0; i < length; i++) {
+            if (exhibits.get(i).getYearAcquired() < year) {
+                firstExhibit = exhibits.get(i);
+            }
+        }
+        return firstExhibit;
+    }
+
+    //function to find the average of all exhibit values//
+
+    public Double findAverageValue() {
+        Double sum = 0.00;
+        Double average = 0.00;
+        Integer length = exhibits.size();
+
+        for (int i = 0; i < length; i++) {
+            sum += exhibits.get(i).getValue();
+        }
+        average = sum/length;
+
+        return average;
+    }
 }
--- coursework/CSC8011_Museum Project/src/csc8011/MuseumIO.java
+++ coursework/CSC8011_Museum Project/src/csc8011/MuseumIO.java
@@ -1,4 +1,93 @@
 package csc8011;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class MuseumIO {
+    public static void main(String[] args) throws FileNotFoundException {
+        Museum museum = new Museum();
+        Scanner scan = new Scanner(System.in);
+        start(museum, scan);
+    }
+
+    public static void start(Museum museum, Scanner scan) throws FileNotFoundException {
+        while (true) {
+            museumOptionMenu(museum, scan);
+            String input = scan.nextLine();
+            read(museum);
+        }
+    }
+
+    public static void read(Museum museum) throws FileNotFoundException {
+        try {
+            Scanner reader = new Scanner(new File("csc8011/exhibits.csv"));
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                if (line.isEmpty()) {
+                continue;
+                }
+                String[] attributes = line.split(",");
+
+                Integer tempId = Integer.parseInt(attributes[0].trim());
+                String tempDesc = attributes[1].trim();
+                Integer tempYear = Integer.parseInt(attributes[2].trim());
+                Double tempValue = Double.parseDouble(attributes[3].trim());
+
+                Exhibit tempExhibit = new Exhibit(tempId,tempDesc,tempYear,tempValue);
+                museum.addExhibit(tempExhibit);
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Error " + e.getMessage());
+        }
+    }
+
+    //museum options function//
+
+    public static void museumOptionMenu(Museum museum, Scanner scan) {
+        System.out.println("Welcome to Museum online portal!");
+        System.out.println("Please enter the number for the corresponding option you wish to see");
+        System.out.println("1: The name of the Museum" +
+                "\n2: A list of all current exhibits and their information" +
+                "\n3: Details of exhibit with the highest value" +
+                "\n4: Details of the first exhibit acquired" +
+                "\n5: The average value of all exhibits");
+
+        switch (scan.nextInt()) {
+            case 1:
+                System.out.println(museum.getMuseumName());
+                System.out.println("Do you wish to change the name?" +
+                        "\n(enter yes to confirm or any other input to return to the menu)");
+
+                if (scan.nextLine().equalsIgnoreCase("yes")) {
+                    System.out.println("Please enter the new museum name");
+                    String nameInput = scan.nextLine();
+                    museum.setMuseumName(nameInput);
+                } else {
+                    break;
+                }
+                break;
+
+            case 2:
+                for (int i = 0; i < museum.exhibits.size(); i++) {
+                    System.out.println(museum.exhibits.get(i));
+                }
+                break;
+
+            case 3:
+                System.out.println("The highest value exhibit is shown below");
+                System.out.println(museum.findHighestValue());
+                break;
+
+            case 4:
+                System.out.println("The first acquired exhibit is shown below");
+                System.out.println(museum.findFirstAdded());
+                break;
+
+            case 5:
+                System.out.println("The average value of all exhibits is " + museum.findAverageValue());
+                break;
+        }
+    }
+
 }
